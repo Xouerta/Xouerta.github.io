a0d55d1b3ca3ecef6e5227c13c6e42565bf72791
Xouerta <2236613237@qq.com>
2024-11-21 11:56:23 +0800

    auto commit

--- bin/autoDeploy.bat
+++ bin/autoDeploy.bat
@@ -1,6 +1,11 @@
 call npm run docs:build
+
 cd docs/.vitepress/dist
+
 git init
+
 git add -A
+
 git commit -m 'deploy'
+
 git push -f https://github.com/Xouerta/Xouerta.github.io.git master:gh-pages
